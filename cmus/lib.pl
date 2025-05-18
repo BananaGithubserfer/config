3986147f19878631643e95978cabeaa9660aca84
vladimir <vladimirshtanenkov@gmail.com>
2025-05-18 13:05:54 +0300

    Разрешен конфликт в .gitignore

---
+++ cmus/lib.pl
@@ -0,0 +1,96 @@
+/home/vladimir/Music/Don't_toy_with_me_miss_nagatoro_op.mp3
+/home/vladimir/Music/Die for metal.mp3
+/home/vladimir/Music/72 Seasons/72 Seasons.mp3
+/home/vladimir/Music/72 Seasons/Chasing Light.mp3
+/home/vladimir/Music/72 Seasons/Crown of Barbed Wire.mp3
+/home/vladimir/Music/72 Seasons/If Darkness Had a Son.mp3
+/home/vladimir/Music/72 Seasons/Inamorata.mp3
+/home/vladimir/Music/72 Seasons/Lux Æterna.mp3
+/home/vladimir/Music/72 Seasons/Room of Mirrors.mp3
+/home/vladimir/Music/72 Seasons/Screaming Suicide.mp3
+/home/vladimir/Music/72 Seasons/Shadows Follow.mp3
+/home/vladimir/Music/72 Seasons/Sleepwalk My Life Away.mp3
+/home/vladimir/Music/72 Seasons/You Must Burn!.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Am I Savage?.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Atlas, Rise!.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Confusion.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Dream No More.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Halo On Fire.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Hardwired.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Here Comes Revenge.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Lords of Summer.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/ManUNkind.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Moth Into Flame.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Murder One.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Now That We're Dead.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Remember Tomorrow.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/Spit Out the Bone.mp3
+/home/vladimir/Music/Hardwired...To Self-Destruct/When a Blind Man Cries.mp3
+/home/vladimir/Music/Kill'em all/[Anesthesia] --Pulling Teeth.mp3
+/home/vladimir/Music/Kill'em all/Hit The Lights.mp3
+/home/vladimir/Music/Kill'em all/Jump In The Fire.mp3
+/home/vladimir/Music/Kill'em all/Motorbreath.mp3
+/home/vladimir/Music/Kill'em all/No Remorse.mp3
+/home/vladimir/Music/Kill'em all/Phantom Lord.mp3
+/home/vladimir/Music/Kill'em all/Seek & Destroy.mp3
+/home/vladimir/Music/Kill'em all/The Four Horsemen.mp3
+/home/vladimir/Music/Kill'em all/Whiplash.mp3
+/home/vladimir/Music/Master of puppets/Battery.mp3
+/home/vladimir/Music/Master of puppets/Damage, Inc.mp3
+/home/vladimir/Music/Master of puppets/Disposable Heroes.mp3
+/home/vladimir/Music/Master of puppets/Leper Messiah.mp3
+/home/vladimir/Music/Master of puppets/Master of Puppets.mp3
+/home/vladimir/Music/Master of puppets/Orion.mp3
+/home/vladimir/Music/Master of puppets/The Thing That Should Not Be.mp3
+/home/vladimir/Music/Master of puppets/Welcome Home (Sanitarium).mp3
+/home/vladimir/Music/Ride The Lightning/Creeping Death.mp3
+/home/vladimir/Music/Ride The Lightning/Escape.mp3
+/home/vladimir/Music/Ride The Lightning/Fade To Black.mp3
+/home/vladimir/Music/Ride The Lightning/Fight Fire With Fire.mp3
+/home/vladimir/Music/Ride The Lightning/For Whom The Bell Tolls.mp3
+/home/vladimir/Music/Ride The Lightning/Ride The Lightning.mp3
+/home/vladimir/Music/Ride The Lightning/The Call Of Ktulu.mp3
+/home/vladimir/Music/Ride The Lightning/Trapped Under Ice.mp3
+/home/vladimir/Music/Animal Magnetism/Animal Magnetisim.mp3
+/home/vladimir/Music/Animal Magnetism/Don't Make No Promises (Your Body Can't Keep).mp3
+/home/vladimir/Music/Animal Magnetism/Falling in Love.mp3
+/home/vladimir/Music/Animal Magnetism/Hold Me Tight.mp3
+/home/vladimir/Music/Animal Magnetism/Lady Starlight.mp3
+/home/vladimir/Music/Animal Magnetism/Make It Real.mp3
+/home/vladimir/Music/Animal Magnetism/Only a Man.mp3
+/home/vladimir/Music/Animal Magnetism/Scorpions - The Zoo.mp3
+/home/vladimir/Music/Animal Magnetism/Twentieth Century Man.mp3
+/home/vladimir/Music/Face The Heat/321.mp3
+/home/vladimir/Music/Face The Heat/Hour I.mp3
+/home/vladimir/Music/Face The Heat/Love Is War.mp3
+/home/vladimir/Music/Face The Heat/Love Will Keep Us Alive.mp3
+/home/vladimir/Music/Face The Heat/Scorpions - Humanity (Official Music Video).mp3
+/home/vladimir/Music/Face The Heat/The Cross.mp3
+/home/vladimir/Music/Face The Heat/The Future Never Dies.mp3
+/home/vladimir/Music/Face The Heat/The Game of Life.mp3
+/home/vladimir/Music/Face The Heat/We Were Born to Fly.mp3
+/home/vladimir/Music/Face The Heat/We Will Rise Again.mp3
+/home/vladimir/Music/Face The Heat/You're Lovin' Me to Death.mp3
+/home/vladimir/Music/Face The Heat/Your Last Song.mp3
+/home/vladimir/Music/Return_to_forever/All for One.mp3
+/home/vladimir/Music/Return_to_forever/Catch Your Luck and Play.mp3
+/home/vladimir/Music/Return_to_forever/Crazy Ride.mp3
+/home/vladimir/Music/Return_to_forever/Dancing with the Moonlight.mp3
+/home/vladimir/Music/Return_to_forever/Delirious.mp3
+/home/vladimir/Music/Return_to_forever/Gypsy Life.mp3
+/home/vladimir/Music/Return_to_forever/Hard Rockin' the Place.mp3
+/home/vladimir/Music/Return_to_forever/House of Cards.mp3
+/home/vladimir/Music/Return_to_forever/One and One Is Three.mp3
+/home/vladimir/Music/Return_to_forever/Rock My Car.mp3
+/home/vladimir/Music/Return_to_forever/Rock 'n' Roll Band.mp3
+/home/vladimir/Music/Return_to_forever/Rollin' Home.mp3
+/home/vladimir/Music/Return_to_forever/Scorpions - Eye of the Storm (Lyric Video).mp3
+/home/vladimir/Music/Return_to_forever/Scorpions - Going out with a Bang (Videoclip).mp3
+/home/vladimir/Music/Return_to_forever/Scorpions - We Built This House (Official Music Video).mp3
+/home/vladimir/Music/Return_to_forever/The Scratch.mp3
+/home/vladimir/Music/Return_to_forever/The World We Used to Know.mp3
+/home/vladimir/Music/Return_to_forever/When the Truth Is a Lie.mp3
+/home/vladimir/Music/Return_to_forever/Who We Are.mp3
+/home/vladimir/Music/Savage Amusement/Don't Stop at the Top.mp3
+/home/vladimir/Music/Savage Amusement/Scorpions - Believe In Love.mp3
+/home/vladimir/Music/Savage Amusement/Scorpions - Passion Rules The Game (Official Video).mp3
